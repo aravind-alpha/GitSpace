a9bc7c4eac8c4c40d23ceb54d6d877bcac0b1014
aravind-alpha <s.aravindviews@gmail.com>
2020-07-18 22:52:44 +0530

    Updated Repos | Chart component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -35,13 +35,30 @@ const Repos = () => {
     })
     .slice(0, 5);
 
+  // Most Stars, Forks
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
   return (
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <Column2D data={mostUsed} />
+        <Column2D data={stars} />
         <Doughnut2D data={mostPopular} />
-        <Bar2D data={mostUsed} />
+        <Bar2D data={forks} />
       </Wrapper>
     </section>
   );
